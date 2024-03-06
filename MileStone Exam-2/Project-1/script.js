f6355f091d7563d37d46daa0f80b3510888e4f93
vipultiwari22 <tiwarivipul155@gmail.com>
2024-03-06 11:54:37 +0530

    update

--- MileStone Exam-2/Project-1/script.js
+++ MileStone Exam-2/Project-1/script.js
@@ -1,1 +1,10 @@
-let btn = document.querySelectorAll('.btns')
+let btn = document.querySelectorAll(".btns");
+let AddBlog = document.querySelector("#btn");
+let popup = document.querySelector(".container");
+let body = document.querySelector(".body");
+console.log(body);
+AddBlog.addEventListener("click", () => {
+  body.classList.toggle("form");
+  //   popup.classList.toggle("form");
+  alert("hllo");
+});
