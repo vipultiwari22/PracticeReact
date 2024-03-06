d47cace83c0a99f599b70c4efa91ffd33e4c4257
vipultiwari22 <tiwarivipul155@gmail.com>
2024-03-06 22:20:04 +0530

    project1 is completed

--- MileStone Exam-2/Project-1/script.js
+++ MileStone Exam-2/Project-1/script.js
@@ -1,10 +1,24 @@
-let btn = document.querySelectorAll(".btns");
 let AddBlog = document.querySelector("#btn");
-let popup = document.querySelector(".container");
-let body = document.querySelector(".body");
-console.log(body);
+let popup = document.querySelector("#form");
+let closeButton = document.querySelector(".Cross");
+let close = document.querySelector(".cross");
+let btns = document.querySelectorAll(".btns");
+let blog = document.querySelector(".blog-conatiner");
+
+btns.forEach((button) => {
+  button.addEventListener("click", () => {
+    blog.classList.toggle("show");
+    // blog.style.display = "block";
+  });
+  close.addEventListener("click", () => {
+    blog.style.display = "none";
+  });
+});
+
 AddBlog.addEventListener("click", () => {
-  body.classList.toggle("form");
-  //   popup.classList.toggle("form");
-  alert("hllo");
+  popup.style.display = "block";
+});
+
+closeButton.addEventListener("click", () => {
+  popup.style.display = "none";
 });
