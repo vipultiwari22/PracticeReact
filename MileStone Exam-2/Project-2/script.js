2826ef1c5fbe9b79b8a128de5b03ea873062691f
Vipul Tiwari <104669764+vipultiwari22@users.noreply.github.com>
2024-03-07 13:46:03 +0530

    Update script.js
    everything is up to date

--- MileStone Exam-2/Project-2/script.js
+++ MileStone Exam-2/Project-2/script.js
@@ -1,47 +1,99 @@
 let boxes = document.querySelectorAll(".boxes");
-let images = document.querySelectorAll("img");
-
-images.forEach((img) => {
-  let childElements = img.children;
-  childElements.forEach((element) => {
-    console.log(element.classList);
-  });
-});
-
-
-let span = document.querySelectorAll("span");
-let btns = document.querySelectorAll("button");
-// console.log(boxes);
 const url = "https://www.omdbapi.com/?s=movie&apikey=d9713940";
+let search = document.querySelector('.search')
+console.log(search);
 
 let fetchMovie = async (url) => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-    // Check if response is successful
-    if (data.Response === "True") {
-      // Iterate over each movie item
-      console.log(data);
-      data.Search.map((movie) => {
-        // console.log(movie);
-        // Here you can access individual properties of each movie
-        // console.log("Title:", movie.Title);
-        // console.log("Year:", movie.Year);
-        // console.log("IMDB ID:", movie.imdbID);
-        // console.log("Type:", movie.Type);
-        // console.log("Poster:", movie.Poster);
-        // image.forEach((img) => {
-        //   let res = img.src;
-        //   console.log(res);
-        // });
-      });
-    } else {
-      console.log("Error:", data.Error);
+
+        // Check if response is successful
+        if (data.Response === "True") {
+            // Iterate over each movie item
+            let movies = data.Search;
+            console.log(data);
+
+            boxes.forEach((ele, index) => {
+                let img = ele.querySelector('img');
+                let span = ele.querySelector('span');
+
+                // Assuming the movies array has the same length as boxes array
+                let movie = movies[index];
+
+                img.src = movie.Poster;
+                span.innerHTML = movie.Title;
+            });
+        } else {
+            console.log("Error:", data.Error);
+        }
     }
-  } catch (error) {
-    console.log("Error fetching data:", error);
-  }
+    catch (error) {
+        console.log("Error fetching data:", error);
+    }
+
+    //  Serching movies we can this method also
+
+    // search.addEventListener('input', async (e) => {
+    //     e.preventDefault()
+    //     const searchTerm = e.target.value.trim()
+    //     if (searchTerm) {
+    //         const searchUrl = `https://www.omdbapi.com/?s=${searchTerm}&apikey=d9713940`;
+    //         try {
+    //             const response = await fetch(searchUrl)
+    //             const data = response.json()
+
+    //             if (data.Response === 'True') {
+    //                 let movie = data.Search;
+    //                 console.log(movie);
+    //             } else {
+    //                 console.log("Error:", data.Error);
+    //             }
+
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     }
+    //     else {
+    //         console.log('please enter value to search');
+    //     }
+    // })
 };
 
-// fetchMovie(url);
+search.addEventListener('input', async (e) => {
+    e.preventDefault();
+
+    const searchTerm = e.target.value.trim();
+
+    if (searchTerm) {
+        const searchUrl = `https://www.omdbapi.com/?s=${searchTerm}&apikey=d9713940`;
+        try {
+            const response = await fetch(searchUrl);
+            const data = await response.json();
+
+            if (data.Response === "True") {
+                let movies = data.Search;
+
+                boxes.forEach((ele, index) => {
+                    let img = ele.querySelector('img');
+                    let span = ele.querySelector('span');
+
+                    // Assuming the movies array has the same length as boxes array
+                    let movie = movies[index];
+
+                    img.src = movie.Poster;
+                    span.innerHTML = movie.Title;
+                });
+            } else {
+                console.log("Error:", data.Error);
+            }
+        } catch (error) {
+            console.log("Error fetching data:", error);
+        }
+    } else {
+        console.log("Please enter a search term");
+    }
+});
+
+fetchMovie(url);
